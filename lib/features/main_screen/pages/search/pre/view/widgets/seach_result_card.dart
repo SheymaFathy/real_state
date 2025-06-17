54105383be6d082e9e2c0bba3b3c19ad0650439c
Hesham <gwassou6@gmail.com>
2025-06-17 14:36:26 +0300

    finish

--- lib/features/main_screen/pages/search/pre/view/widgets/seach_result_card.dart
+++ lib/features/main_screen/pages/search/pre/view/widgets/seach_result_card.dart
@@ -11,6 +11,7 @@ import '../../../../../../../core/helper/routes.dart';
 import '../../../../../../../core/shared_preferences/cach_helper.dart';
 import '../../../../../../widgets/elevated_button_def.dart';
 import '../../../../../../widgets/title_text.dart';
+import '../../../../home/pre/view/unit_details.dart';
 import '../../../../my_fav/data/repo/favorite_repo.dart';
 import '../../../../my_fav/pre/view_model/my_favorite_cubit.dart';
 import '../../../../my_fav/pre/view_model/my_favorite_state.dart';
@@ -24,13 +25,13 @@ class SearchResultCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => FavoriteCubit(FavoriteRepository()),
-      child: SearchResultCardContent(data: data),
-    );
+      child: SearchResultCardContent(data: data));
   }
 }
 
 class SearchResultCardContent extends StatefulWidget {
   final Data data;
+
   const SearchResultCardContent({super.key, required this.data});
 
   @override
@@ -46,7 +47,6 @@ class _SearchResultCardContentState extends State<SearchResultCardContent> {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     final loc = context.loc;
-
     return SizedBox(
       width: screenWidth * 0.8,
       child: Stack(
@@ -186,12 +186,20 @@ class _SearchResultCardContentState extends State<SearchResultCardContent> {
                   padding: const EdgeInsets.all(8.0),
                   child: ElevatedButtonDef(
                     press: () {
-                      context.push(
-                        AppRoutes.unitDetails.replaceFirst(
-                          ':id',
-                          widget.data.id.toString(),
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder:
+                              (context) =>
+                                  UnitDetailsPage(unitId: widget.data.id!),
                         ),
                       );
+                      // context.push(
+                      //   AppRoutes.unitDetails.replaceFirst(
+                      //     ':id',
+                      //     widget.data.id.toString(),
+                      //   ),
+                      // );
                     },
                     text: 'Learn More',
                   ),
@@ -205,9 +213,7 @@ class _SearchResultCardContentState extends State<SearchResultCardContent> {
             child: BlocConsumer<FavoriteCubit, FavoriteState>(
               listener: (context, state) {
                 if (state is FavoriteSuccess) {
-                  setState(() {
-
-                  });
+                  setState(() {});
                 }
               },
               builder: (context, state) {
@@ -221,12 +227,18 @@ class _SearchResultCardContentState extends State<SearchResultCardContent> {
                         shape: BoxShape.circle,
                         color: AppColors.backGround(context),
                         boxShadow: [
-                          BoxShadow(color: AppColors.black(context), blurRadius: 4),
+                          BoxShadow(
+                            color: AppColors.black(context),
+                            blurRadius: 4,
+                          ),
                         ],
                       ),
                       child: Icon(
-                        widget.data.isFavorite! ? Icons.favorite : Icons.favorite_border,
-                        color: widget.data.isFavorite! ? Colors.red : Colors.red,
+                        widget.data.isFavorite!
+                            ? Icons.favorite
+                            : Icons.favorite_border,
+                        color:
+                            widget.data.isFavorite! ? Colors.red : Colors.red,
                         size: 24,
                       ),
                     ),
@@ -234,37 +246,47 @@ class _SearchResultCardContentState extends State<SearchResultCardContent> {
                       final token = CacheHelper.getSaveData(key: "token");
 
                       if (token != null && token.isNotEmpty) {
-                        context.read<FavoriteCubit>().addToFavorite(widget.data.id.toString());
+                        context.read<FavoriteCubit>().addToFavorite(
+                          widget.data.id.toString(),
+                        );
                       } else {
-
                         showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              backgroundColor: AppColors.backGround(context),
-                              title: TitleText(title: loc.translate("alert")),
-                              content: TitleText(title: loc.translate("to_add_favorite"),),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              actions: [
-                                TextButton(
-                                  onPressed: () => Navigator.pop(context),
-                                  child: TitleText(title: loc.translate("cancel")),
+                          context: context,
+                          builder:
+                              (context) => AlertDialog(
+                                backgroundColor: AppColors.backGround(context),
+                                title: TitleText(title: loc.translate("alert")),
+                                content: TitleText(
+                                  title: loc.translate("to_add_favorite"),
                                 ),
-                                ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: AppColors.primary(context),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: TitleText(
+                                      title: loc.translate("cancel"),
                                     ),
                                   ),
-                                  onPressed: () {
-                                    context.go(AppRoutes.login);
-                                  },
-                                  child: TitleText(title: loc.translate("login"),),
-                                ),
-                              ],
-                            )
+                                  ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: AppColors.primary(
+                                        context,
+                                      ),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      context.go(AppRoutes.login);
+                                    },
+                                    child: TitleText(
+                                      title: loc.translate("login"),
+                                    ),
+                                  ),
+                                ],
+                              ),
                         );
                       }
                     },
@@ -273,9 +295,6 @@ class _SearchResultCardContentState extends State<SearchResultCardContent> {
               },
             ),
           ),
-
-
-
 
           // .......................
           Positioned(
@@ -286,8 +305,9 @@ class _SearchResultCardContentState extends State<SearchResultCardContent> {
               child: InkWell(
                 borderRadius: BorderRadius.circular(16),
                 onTap: () {
-                  context.read<FavoriteCubit>().addToFavorite(widget.data.id.toString());
-
+                  context.read<FavoriteCubit>().addToFavorite(
+                    widget.data.id.toString(),
+                  );
                 },
                 child: Container(
                   padding: const EdgeInsets.all(8.0),
@@ -318,7 +338,6 @@ class _SearchResultCardContentState extends State<SearchResultCardContent> {
               ),
             ),
           ),
-
         ],
       ),
     );
